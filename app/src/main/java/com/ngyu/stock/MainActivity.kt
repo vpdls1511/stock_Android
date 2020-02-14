ef2a935f275cd9bc2b00166200755f0024cd2c0d
김남규 <kimnamkyu@Kim.local>
2020-02-14 16:39:26 +0900

    delete ad

--- app/src/main/java/com/ngyu/stock/MainActivity.kt
+++ app/src/main/java/com/ngyu/stock/MainActivity.kt
@@ -16,10 +16,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        MobileAds.initialize(this, "ca-app-pub-2804563531468153~7370681143")
-        val adView = AdView(this)
-        adView.adSize = AdSize.BANNER
-        adView.adUnitId = "ca-app-pub-3940256099942544/6300978111"
 
         val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
         val pager = findViewById<ViewPager>(R.id.container)
