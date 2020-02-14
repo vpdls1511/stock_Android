c45c36557dd4694099557bec8514eba4f8617051
김남규 <kimnamkyu@Kim.local>
2020-02-14 18:27:57 +0900

    issue : NullPointerException

--- app/src/main/java/com/ngyu/stock/MainActivity.kt
+++ app/src/main/java/com/ngyu/stock/MainActivity.kt
@@ -3,14 +3,9 @@ package com.ngyu.stock
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager.widget.ViewPager
-import com.google.android.gms.ads.AdSize
 import com.google.android.material.tabs.TabLayout
-import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var mAdView : AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/ngyu/stock/fragments/AverageFragment.kt
+++ app/src/main/java/com/ngyu/stock/fragments/AverageFragment.kt
@@ -1,11 +1,14 @@
 package com.ngyu.stock.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ngyu.stock.R
+import kotlinx.android.synthetic.main.average_stock.*
 
 open class AverageFragment: Fragment()
 {
@@ -22,6 +25,23 @@ open class AverageFragment: Fragment()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
         val v = inflater.inflate(R.layout.average_stock, container, false)
+
+
+            quantity2.addTextChangedListener(object : TextWatcher{
+                override fun afterTextChanged(p0: Editable?) {}
+
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    val num1: Int = Integer.parseInt(until_pirce1.toString())
+                    val num2: Int = Integer.parseInt(quantity1.toString())
+                    val num3: Int = Integer.parseInt(until_pirce2.toString())
+                    val num4: Int = Integer.parseInt(quantity2.toString())
+                    val result = ((num1 * num2) + (num3 * num4)) / (num2 + num4)
+                    averagePrice.setText(result)
+                }
+            })
+
         return v
     }
+
 }
