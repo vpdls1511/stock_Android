bd1a14d882ec82a3c0fc5abf124c925865ae268d
김남규 <kimnamkyu@Kim.local>
2020-02-19 15:39:56 +0900

    radio button added

--- app/src/main/java/com/ngyu/stock/ui/fragments/ContinuityFragment.kt
+++ app/src/main/java/com/ngyu/stock/ui/fragments/ContinuityFragment.kt
@@ -5,6 +5,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import com.gauravk.bubblenavigation.BubbleNavigationLinearView
@@ -27,6 +30,20 @@ open class ContinuityFragment : Fragment()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
         val v = inflater.inflate(R.layout.continuity_stock, container, false)
+
+        var radioValue = 0
+        val radioGroup: RadioGroup = v.findViewById(R.id.radioGroup)
+
+        radioGroup.setOnCheckedChangeListener{group, checkedId ->
+            val radioButton:RadioButton = v.findViewById(checkedId)
+            Toast.makeText(context, radioButton.text ,Toast.LENGTH_SHORT).show()
+
+            if(radioButton.text == "연속 상한가"){
+                radioValue = 1
+            }else if(radioButton.text == "연속 하한가"){
+                radioValue = 0
+            }
+        }
 
 
 
